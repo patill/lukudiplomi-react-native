4c819b26d2bf387b5fb5f3d2b909c0d107bd49fc
Miika Koskela <koskela.miika.s@outlook.com>
2019-07-06 20:15:51 +0300

    Import config url from another file

--- src/Datasources/ConfigDatasource.js
+++ src/Datasources/ConfigDatasource.js
@@ -7,6 +7,7 @@
  */
 
 import { AsyncStorage } from 'react-native';
+import configUri from './../../config';
 
 /**
  * Configuration data source.
@@ -18,83 +19,91 @@ import { AsyncStorage } from 'react-native';
  */
 class ConfigDatasource {
 
-  // This class depends on downloader and URI (which is hardcoded currently)
-  constructor (downloader) {
-    this.uri = 'https://www.example.com/lukudiplomi-config.txt';
-    this.downloader = downloader;
-  }
-
-  /**
-   * Get configuration data for given grade.
-   *
-   * @param string grade
-   * @returns object
-   */
-  getConfigFor = async (grade) => {
-    let data,
-      json,
-      current;
-    try {
-      data = await this.getConfig();
-      json = JSON.parse(data);
-      current = json.find((entry) => {
-        return entry.title === grade;
-      });
-    } catch (error) {
-
-    }
-    return current;
-  }
-
-  // Get all grade names, e.g. "7. luokka"
-  getGrades = async () => {
-    let data = null;
-    try {
-      data = await this.getConfig();
-    } catch (error) {
-      console.log(error);
+    // This class depends on downloader and URI
+    constructor(downloader) {
+        this.uri = configUri;
+        this.downloader = downloader;
     }
 
-    return data.map(item => item.title);
-  }
-
-  // Get app configuration data, e.g. grades, their book and data URLs
-  getConfig = async () => {
-    let data = null;
-    try {
-      // First try local storage; if it's there, it's in JSON string format
-      data = await AsyncStorage.getItem('lukudiplomi/asetukset');
-
-      if (!data) {
-        // If it's not there, download and make it JSON string format and store
-        // it locally.
-        data = JSON.stringify(await this.downloader.getData(this.uri));
-        await AsyncStorage.setItem('lukudiplomi/asetukset', data);
-      }
-      // This is just to ensure real JSON object.
-      data = JSON.parse(data);
-    } catch (error) {
-
+    /**
+     * Get configuration data for given grade.
+     *
+     * @param string grade
+     * @returns object
+     */
+    getConfigFor = async (grade) => {
+        let data,
+            json,
+            current;
+        try {
+            data = await this.getConfig();
+            json = JSON.parse(data);
+            current = json.find((entry) => {
+                return entry.title === grade;
+            });
+        } catch (error) {
+            consoel.log(error);
+        }
+        return current;
     }
 
-    return data;
-  }
+    /**
+     * Get all grade names, e.g. "7. luokka"
+     *
+     * @returns Promise, which resolves to array of strings or an empty array
+     **/
+    getGrades = async () => {
+        let data = [];
+        try {
+            // Returns Promise which resolves to JSON object
+            data = await this.getConfig();
+        } catch (error) {
+            console.log("[ConfigDatasource::getGrades]: " + error);
+        }
 
-  /**
-   * Get the currently selected grade.
-   *
-   * If there's no such set, the value is null and it's returned. It's the
-   * callers responsibility to check what this returns.
-   */
-  getCurrentGrade = async () => {
-    let data = null;
-    try {
-      data = await AsyncStorage.getItem('lukudiplomi/aste');
-    } catch (error) {
-      console.log(error);
+        return data.map(item => item.title);
     }
-    return data;
-  }
+
+    /**
+     * Get app configuration data, e.g. grades, their book and data URLs
+     *
+     * Try first local AsyncStorage, if not available from there, try downloading.
+     *
+     * @returns Promise, which resolves to JSON object
+     **/
+    getConfig = async () => {
+        let data = [];
+        try {
+            data = await AsyncStorage.getItem('lukudiplomi/asetukset'); // returns string
+            if (!data) {
+                data = await this.downloader.getData(this.uri); // returns string
+                if (data) {
+                    await AsyncStorage.setItem('lukudiplomi/asetukset', data);
+                }
+            }
+        } catch (error) {
+            console.log("[ConfigDatasource::getConfig]: " + error);
+        }
+
+        data = JSON.parse(data);
+        return data;
+    }
+
+    /**
+     * Get the currently selected grade.
+     *
+     * @returns Promise which resolves as a string
+     */
+    getCurrentGrade = async () => {
+        let data = "";
+        try {
+            data = await AsyncStorage.getItem('lukudiplomi/aste');
+        } catch (error) {
+            console.log("[ConfigDatasource::getCurrentGrade]: " + error);
+        }
+
+        return data;
+    }
 }
 
 export default ConfigDatasource;
