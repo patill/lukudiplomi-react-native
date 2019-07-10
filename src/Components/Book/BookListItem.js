eaf4b7cebaec227950dbb36c1a15c5a745b9968e
Miika Koskela <koskela.miika.s@outlook.com>
2019-07-10 20:42:32 +0300

    Prettier

--- src/Components/Book/BookListItem.js
+++ src/Components/Book/BookListItem.js
@@ -3,14 +3,14 @@
  * @copyright Tampereen Kaupunginkirjasto, 2018-
  * @license MIT (see LICENSE)
  */
-import React, { PureComponent } from 'react';
-import { Text, View, Image, TouchableOpacity } from 'react-native';
-import PropTypes from 'prop-types';
-import { Icon } from 'react-native-elements';
-import Styles, { Colors } from './Styles';
+import React, { PureComponent } from "react";
+import { Text, View, Image, TouchableOpacity } from "react-native";
+import PropTypes from "prop-types";
+import { Icon } from "react-native-elements";
+import Styles, { Colors } from "./Styles";
 
 export default class BookListItem extends PureComponent {
-  constructor (props) {
+  constructor(props) {
     super(props);
 
     // This is a flag to prevent updating book list when returning from book
@@ -22,8 +22,8 @@ export default class BookListItem extends PureComponent {
     };
   }
 
-  getIcon (isBookmarked, onPress) {
-    let icon = isBookmarked ? 'star' : 'star-outline';
+  getIcon(isBookmarked, onPress) {
+    let icon = isBookmarked ? "star" : "star-outline";
     let color = isBookmarked ? Colors.Yellow : Colors.DarkGrey;
     return (
       <Icon
@@ -31,15 +31,15 @@ export default class BookListItem extends PureComponent {
         type="material-community"
         size={32}
         color={color}
-        underlayColor={'rgba(255, 255, 255, 0)'}
+        underlayColor={"rgba(255, 255, 255, 0)"}
         onPress={onPress}
         style={Styles.bookmark}
       />
     );
   }
 
-  render () {
-    let route = this.props.route ? this.props.route : 'BookDetails';
+  render() {
+    let route = this.props.route ? this.props.route : "BookDetails";
     return (
       <TouchableOpacity
         onPress={() => {
@@ -53,7 +53,7 @@ export default class BookListItem extends PureComponent {
             <Image
               source={{
                 uri: decodeURIComponent(this.props.item.coverImage),
-                cache: 'force-cache'
+                cache: "force-cache"
               }}
               style={Styles.bookCoverImage}
             />
@@ -61,14 +61,14 @@ export default class BookListItem extends PureComponent {
           <View style={Styles.bookInfoView}>
             <Text style={Styles.heading}>{this.props.item.title}</Text>
             <Text style={Styles.subheading}>{this.props.item.author}</Text>
-            <Text style={Styles.subheading}>{this.props.item.tags.join(', ')}</Text>
+            <Text style={Styles.subheading}>
+              {this.props.item.tags.join(", ")}
+            </Text>
           </View>
           <View style={Styles.bookmarkContainer}>
-            {
-              this.getIcon(this.props.item.isBookmarked, () => {
-                this.props.onBookmark(this.props.item);
-              })
-            }
+            {this.getIcon(this.props.item.isBookmarked, () => {
+              this.props.onBookmark(this.props.item);
+            })}
           </View>
         </View>
       </TouchableOpacity>
--- src/Components/Book/BookListItemSelect.js
+++ src/Components/Book/BookListItemSelect.js
@@ -3,31 +3,30 @@
  * @copyright Tampereen Kaupunginkirjasto, 2018-
  * @license MIT (see LICENSE)
  */
-import React, { PureComponent } from 'react';
-import { Text, View, Image, TouchableOpacity } from 'react-native';
-import PropTypes from 'prop-types';
-import { Icon } from 'react-native-elements';
-import Styles from './Styles';
+import React, { PureComponent } from "react";
+import { Text, View, Image, TouchableOpacity } from "react-native";
+import PropTypes from "prop-types";
+import { Icon } from "react-native-elements";
+import Styles from "./Styles";
 
 export default class BookListItemSelect extends PureComponent {
-
   /**
    * Get an icon based on whether item is selected or not.
    *
    * @param {boolean} checked - Item status, i.e. whether it's selected or not.
    */
-  getIcon = (checked) => {
-    let name = checked ? 'checkbox-marked' : 'checkbox-blank-outline';
+  getIcon = checked => {
+    let name = checked ? "checkbox-marked" : "checkbox-blank-outline";
     return (
       <Icon
         name={name}
         type="material-community"
         size={24}
-        underlayColor={'rgba(255, 255, 255, 0)'}
+        underlayColor={"rgba(255, 255, 255, 0)"}
         style={Styles.bookmark}
       />
     );
-  }
+  };
 
   /**
    * Respond to item press.
@@ -37,35 +36,35 @@ export default class BookListItemSelect extends PureComponent {
    */
   onPress = () => {
     this.props.onPress(this.props.item);
-  }
+  };
 
   render = () => {
     return (
-        <View style={Styles.bookListRowItem}>
-          <View style={Styles.bookCoverImageView}>
-            <Image
-              source={{
-                uri: decodeURIComponent(this.props.item.coverImage),
-                cache: 'force-cache'
-              }}
-              style={Styles.bookCoverImage}
-            />
-          </View>
-          <View style={Styles.bookInfoView}>
-            <Text style={Styles.heading}>{this.props.item.title}</Text>
-            <Text style={Styles.subheading}>{this.props.item.author}</Text>
-            <Text style={Styles.subheading}>{this.props.item.tags.join(', ')}</Text>
-          </View>
-          <View style={Styles.bookmarkContainer}>
-            <TouchableOpacity
-              onPress={this.onPress}
-            >
-              {this.getIcon(this.props.selected)}
-            </TouchableOpacity>
-          </View>
+      <View style={Styles.bookListRowItem}>
+        <View style={Styles.bookCoverImageView}>
+          <Image
+            source={{
+              uri: decodeURIComponent(this.props.item.coverImage),
+              cache: "force-cache"
+            }}
+            style={Styles.bookCoverImage}
+          />
         </View>
+        <View style={Styles.bookInfoView}>
+          <Text style={Styles.heading}>{this.props.item.title}</Text>
+          <Text style={Styles.subheading}>{this.props.item.author}</Text>
+          <Text style={Styles.subheading}>
+            {this.props.item.tags.join(", ")}
+          </Text>
+        </View>
+        <View style={Styles.bookmarkContainer}>
+          <TouchableOpacity onPress={this.onPress}>
+            {this.getIcon(this.props.selected)}
+          </TouchableOpacity>
+        </View>
+      </View>
     );
-  }
+  };
 }
 
 BookListItemSelect.propTypes = {
