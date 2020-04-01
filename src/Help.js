f3dc3716c4b74fc718f74feb83b8e1ce11ae161b
Till Paala <till.paala@pori.fi>
2020-04-01 12:20:40 +0300

    Update Help.js

--- src/Help.js
+++ src/Help.js
@@ -20,33 +20,33 @@ export default class Help extends Component {
     return (
       <ScrollView style={Styles.mainContainer}>
         <View style={Styles.container}>
-          <Text style={Styles.heading}>Tervetuloa Pirkanmaan lukudiplomi -sovelluksen käyttäjäksi</Text>
-          <Text style={Styles.text}>Sovellus on tarkoitettu yläkoulun, lukion ja ammattioppilaitosten opiskelijoille, jotka suorittavat Pirkanmaan lukudiplomia. Mukautettu lukudiplomi on tarkoitettu oppilaille, joille lukeminen on eri syistä vaikeaa.</Text>
+          <Text style={Styles.heading}>Tervetuloa Porin lukudiplomi -sovelluksen käyttäjäksi</Text>
+          <Text style={Styles.text}>Sovellus on tarkoitettu kaikenikäisille koululaisille, jotka suorittavat Porin lukudiplomia.</Text>
 
-          <Text style={Styles.text}>Valitse ensin oma luokka-asteesi. Siirtyessäsi luokalta toiselle voit vaihtaa valittua luokkaa.</Text>
+
 
           <Text style={Styles.heading}>Listat auttavat löytämään lukemista</Text>
-          <Text style={Styles.text}>Sovi lukudiplomin suoritustavoista opettajasi kanssa. Kirjoja on tarkoitus valita luettavaksi niin, että eri lajit tulevat tutuiksi.</Text> 
+          <Text style={Styles.text}>Lukudiplomin kirjat on jaettu aiheen mukaan aihelistoihin. Valitse kirjoja eri listoilta. Sovi lukudiplomin suoritustavoista opettajasi kanssa. Kirjoja on tarkoitus valita luettavaksi niin, että eri lajit tulevat tutuiksi.</Text>
 
-          <Text style={Styles.text}>Jokaisella luokka-asteella on oma kirjavalikoimansa. Kunkin luokka-asteen kirjalistoja voi rajata näytettäväksi lajeittain. Joitain teoksia on saatavana myös e-kirjoina.</Text>
+
 
           <Text style={Styles.heading}>Kuinka monta luetaan?</Text>
-          <Text style={Styles.text}>Yläkoulun luokilla 7–9 luetaan seitsemän kirjaa lukuvuoden aikana.</Text>
+          <Text style={Styles.text}></Text>
 
-          <Text style={Styles.text}>Toisen asteen lukudiplomia varten luetaan 12 kirjaa koko opiskeluajan kuluessa.</Text>
+          <Text style={Styles.text}></Text>
 
-          <Text style={Styles.text}>Mukautetun diplomin suorittaja sopii kirjojen määrästä yhdessä opettajansa kanssa.</Text>
+          <Text style={Styles.text}></Text>
 
           <Text style={Styles.heading}>Tehtävät</Text>
-          <Text style={Styles.text}>Jokaisella luokka-asteella on omat tehtävänsä. Jokaisesta luetusta kirjasta tehdään yksi valinnainen tehtävä.</Text> 
+          <Text style={Styles.text}>Jokaisella luokka-asteella on omat tehtävänsä. Jokaisesta luetusta kirjasta tehdään yksi valinnainen tehtävä.</Text>
 
           <Text style={Styles.text}>On myös mahdollista vertailla kirjoja tai muuten yhdistää useampi kirja samaan tehtävään. Valitse mieluummin erilaisia tehtäviä. Sovi opettajasi kanssa tehtävien suoristustapa.</Text>
 
-          <Text style={Styles.text}>Sovelluksessa voit ylläpitää muistilistaa tehdyistä tehtävistä ja niihin valituista kirjoista. Voit tallentaa muistilistan tekstitiedostoksi laitteellesi ja halutessasi jakaa sen opettajan ohjeiden mukaisesti, esimerkiksi sähköpostilla.</Text> 
+          <Text style={Styles.text}>Sovelluksessa voit ylläpitää muistilistaa tehdyistä tehtävistä ja niihin valituista kirjoista. Voit tallentaa muistilistan tekstitiedostoksi laitteellesi ja halutessasi jakaa sen opettajan ohjeiden mukaisesti, esimerkiksi sähköpostilla.</Text>
 
 
           <Text style={Styles.heading}>Saatavuus</Text>
-          <Text style={Styles.text}>Kirjan esittelysivulta pääset PIKI-verkkokirjastoon, jossa voit tarkastaa kirjan saatavuuden Pirkanmaan kirjastoissa ja halutessasi varata sen.</Text> 
+          <Text style={Styles.text}>Kirjan esittelysivulta pääset Satakirjastojen verkkokirjastoon, jossa voit tarkastaa kirjan saatavuuden Satakunnan kirjastoissa ja halutessasi varata sen.</Text>
 
           <Text style={Styles.text}>Verkkokirjastosta pääset myös e-kirjapalveluun, jossa e-kirjoja lainataan ja varataan.</Text>
 
@@ -56,14 +56,14 @@ export default class Help extends Component {
           <Text style={Styles.text}>Kun olet lukenut kirjat ja tehnyt opettajan kanssa sovitut tehtävät, saat opettajalta lukudiplomin.</Text>
 
           <Text style={Styles.text}>Onnea lukuharrastuksesi parissa!</Text>
-          
+
           <Text style={Styles.heading}>Lisätietoja</Text>
           <Text style={Styles.text}>Tampereen kaupunginkirjasto, pääkirjasto Metson lasten ja nuorten osasto</Text>
 
           <Text style={Styles.heading}>Tekninen suunnittelu ja toteutus</Text>
-          <Text style={Styles.text}>Miika Koskela (iOS ja Android versio 2.0)</Text> 
+          <Text style={Styles.text}>Miika Koskela (iOS ja Android versio 2.0)</Text>
           <Text style={Styles.text}>Samuli Puolakka (Android, versio 1.0)</Text>
-          
+
           <Text style={Styles.heading}>Yhteistyössä:</Text>
           <View style={Styles.collaboratorContainer}>
             <Image
